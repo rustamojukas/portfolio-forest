0d5411c17fae0dab510b0846adb27395ece01066
Rustam Ojukas <rustam.ojukas@gmail.com>
2016-10-15 14:54:33 +0300

    index, login pages flipped

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,14 @@
 (function() {
   'use strict';
-
+  //.login-button
+  $(".login-button").on( "click", function() {
+    console.log( "Click" );
+    $('.welcome__user-card-wrapper').addClass('flipped');
+    $('.login-button').hide();
+  });
+  $(".home").on( "click", function() {
+    console.log( "Click" );
+    $('.welcome__user-card-wrapper').removeClass('flipped');
+    $('.login-button').show();
+  });
 })();
